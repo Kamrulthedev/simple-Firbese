7b632dd903544e3f6d99ed88275c1e6b01f64de0
Kamrul Hassan <kamrulthedev@gmail.com>
2023-11-21 11:20:05 +0600

    Provider-commit

--- src/__steps.js
+++ src/__steps.js
@@ -5,4 +5,10 @@
 * 4. install firbase : npm install firebase
 * 5. add config file to your project
 * 6. DANGER: Do not publish or make firbase config to pulic by pushing those to github
+* 7. Visit: Go to publish or make firebase config to public by pushing those to github.
+* 8. exprot app from the firbase.config.js file : exprot default app
+* 9. Login.jsx : improt getAuth from firbase/auth 
+* 10. crate const auth = getAuth(app)
+* 11. improt googleAuthProvider and create a new Provider
+* 12. usesignInWithPopUp and pass auth and provider
 * */ 
